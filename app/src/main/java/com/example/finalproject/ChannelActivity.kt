b999da95976be6d5eea2218a2828da15cd00a6b8
Amiel Christian Mala-ay <88815448+amielchristian@users.noreply.github.com>
2023-11-25 19:03:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/finalproject/ChannelActivity.kt
@@ -0,0 +1,91 @@
+package com.example.finalproject
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.addCallback
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.finalproject.databinding.ActivityChannelBinding
+import io.getstream.chat.android.models.Channel
+import io.getstream.chat.android.ui.common.state.messages.Edit
+import io.getstream.chat.android.ui.common.state.messages.MessageMode
+import io.getstream.chat.android.ui.viewmodel.messages.MessageComposerViewModel
+import io.getstream.chat.android.ui.viewmodel.messages.MessageListHeaderViewModel
+import io.getstream.chat.android.ui.viewmodel.messages.MessageListViewModel
+import io.getstream.chat.android.ui.viewmodel.messages.MessageListViewModelFactory
+import io.getstream.chat.android.ui.viewmodel.messages.bindView
+
+class ChannelActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChannelBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Step 0 - inflate binding
+        binding = ActivityChannelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val cid = checkNotNull(intent.getStringExtra(CID_KEY)) {
+            "Specifying a channel id is required when starting ChannelActivity"
+        }
+
+        // Step 1 - Create three separate ViewModels for the views so it's easy
+        //          to customize them individually
+        val factory = MessageListViewModelFactory(this, cid)
+        val messageListHeaderViewModel: MessageListHeaderViewModel by viewModels { factory }
+        val messageListViewModel: MessageListViewModel by viewModels { factory }
+        val messageComposerViewModel: MessageComposerViewModel by viewModels { factory }
+
+        // TODO set custom Imgur attachment factory
+
+        // Step 2 - Bind the view and ViewModels, they are loosely coupled so it's easy to customize
+        messageListHeaderViewModel.bindView(binding.messageListHeaderView, this)
+        messageListViewModel.bindView(binding.messageListView, this)
+        messageComposerViewModel.bindView(binding.messageComposerView, this)
+
+        // Step 3 - Let both MessageListHeaderView and MessageComposerView know when we open a thread
+        messageListViewModel.mode.observe(this) { mode ->
+            when (mode) {
+                is MessageMode.MessageThread -> {
+                    messageListHeaderViewModel.setActiveThread(mode.parentMessage)
+                    messageComposerViewModel.setMessageMode(MessageMode.MessageThread(mode.parentMessage))
+                }
+
+                is MessageMode.Normal -> {
+                    messageListHeaderViewModel.resetThread()
+                    messageComposerViewModel.leaveThread()
+                }
+            }
+        }
+
+        // Step 4 - Let the message input know when we are editing a message
+        binding.messageListView.setMessageEditHandler { message ->
+            messageComposerViewModel.performMessageAction(Edit(message))
+        }
+
+        // Step 5 - Handle navigate up state
+        messageListViewModel.state.observe(this) { state ->
+            if (state is MessageListViewModel.State.NavigateUp) {
+                finish()
+            }
+        }
+
+        // Step 6 - Handle back button behaviour correctly when you're in a thread
+        val backHandler = {
+            messageListViewModel.onEvent(MessageListViewModel.Event.BackButtonPressed)
+        }
+        binding.messageListHeaderView.setBackButtonClickListener(backHandler)
+        onBackPressedDispatcher.addCallback(this) {
+            backHandler()
+        }
+    }
+
+    companion object {
+        private const val CID_KEY = "key:cid"
+
+        fun newIntent(context: Context, channel: Channel): Intent =
+            Intent(context, ChannelActivity::class.java).putExtra(CID_KEY, channel.cid)
+    }
+}
